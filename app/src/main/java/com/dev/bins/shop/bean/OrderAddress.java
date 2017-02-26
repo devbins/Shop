1a1da3697d84a9dc537761843412881512fce0ce
bin <bin6160@126.com>
2017-02-26 22:23:27 +0800

    add address bean

---
+++ app/src/main/java/com/dev/bins/shop/bean/OrderAddress.java
@@ -0,0 +1,55 @@
+package com.dev.bins.shop.bean;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by bin on 26/02/2017.
+ */
+
+public class OrderAddress extends DataSupport {
+    private long id;
+    private String name;
+    private String phone;
+    private String add;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAdd() {
+        return add;
+    }
+
+    public void setAdd(String add) {
+        this.add = add;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
