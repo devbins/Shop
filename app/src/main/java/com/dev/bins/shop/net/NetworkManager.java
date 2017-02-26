c88ed6321e46c3c8440ca035f5aea2161ac7e370
bin <bin6160@126.com>
2017-02-27 07:59:05 +0800

    refactor net request

--- app/src/main/java/com/dev/bins/shop/net/NetworkManager.java
+++ app/src/main/java/com/dev/bins/shop/net/NetworkManager.java
@@ -11,10 +11,13 @@ import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import rx.Observable;
 import rx.Subscriber;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 
 /**
  * Created by bin on 24/02/2017.
@@ -45,43 +48,57 @@ public class NetworkManager {
 
 
     public Subscription getBanner(Subscriber<List<Banner>> subscriber) {
-        return mApi.getBanner()
-                .subscribeOn(Schedulers.io())
-                .unsubscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(subscriber);
+//        return mApi.getBanner()
+//                .subscribeOn(Schedulers.io())
+//                .unsubscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(subscriber);
+        return getData(mApi.getBanner(), subscriber);
     }
 
-    public Subscription getRecommend(Subscriber<List<Recommend>> subscriber){
-        return mApi.getRecommend()
-                .subscribeOn(Schedulers.io())
-                .unsubscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(subscriber);
+    public Subscription getRecommend(Subscriber<List<Recommend>> subscriber) {
+//        return mApi.getRecommend()
+//                .subscribeOn(Schedulers.io())
+//                .unsubscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(subscriber);
+        return getData(mApi.getRecommend(), subscriber);
     }
 
-    public Subscription getGoods(Subscriber<Goods> subscriber,int page,int pageSize){
-        return mApi.getGoodsWithPage(page,pageSize)
-                .subscribeOn(Schedulers.io())
-                .unsubscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(subscriber);
-    }
-    public Subscription getCategory(Subscriber<List<Category>> subscriber){
-        return mApi.getCategory()
-                .subscribeOn(Schedulers.io())
-                .unsubscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(subscriber);
-    }
-    public Subscription getCategoryData(Subscriber<Goods> subscriber,int cid,int curPage,int pageSize){
-        return mApi.getCategoryData(cid,curPage,pageSize)
-                .subscribeOn(Schedulers.io())
-                .unsubscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(subscriber);
+    public Subscription getGoods(Subscriber<Goods> subscriber, int page, int pageSize) {
+//        return mApi.getGoodsWithPage(page, pageSize)
+//                .subscribeOn(Schedulers.io())
+//                .unsubscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(subscriber);
+        return getData(mApi.getGoodsWithPage(page, pageSize), subscriber);
     }
 
+    public Subscription getCategory(Subscriber<List<Category>> subscriber) {
+//        return mApi.getCategory()
+//                .subscribeOn(Schedulers.io())
+//                .unsubscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(subscriber);
+        return getData(mApi.getCategory(), subscriber);
+    }
+
+    public Subscription getCategoryData(Subscriber<Goods> subscriber, int cid, int curPage, int pageSize) {
+//        return mApi.getCategoryData(cid,curPage,pageSize)
+//                .subscribeOn(Schedulers.io())
+//                .unsubscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(subscriber);
+        return getData(mApi.getCategoryData(cid, curPage, pageSize), subscriber);
+    }
+
+    public <T> Subscription getData(Observable<T> observable, Subscriber<T> subscriber) {
+        return observable.subscribeOn(Schedulers.io())
+                .unsubscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(subscriber);
+
+    }
 
 
 }
