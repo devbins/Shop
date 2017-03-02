a58178e38f7ec4968f57fcaf9fd321029acd4280
bin <bin6160@126.com>
2017-03-02 22:45:10 +0800

    init reg layout

--- app/src/main/java/com/dev/bins/shop/RegisterActivity.java
+++ app/src/main/java/com/dev/bins/shop/RegisterActivity.java
@@ -2,12 +2,38 @@ package com.dev.bins.shop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-public class RegisterActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.et_phone)
+    EditText mEditTextPhone;
+    @BindView(R.id.et_pwd)
+    EditText mEditTextPwd;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        ButterKnife.bind(this);
+        mEditTextPhone.setOnClickListener(this);
+        mEditTextPwd.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.et_phone:
+
+                break;
+            case R.id.et_pwd:
+                break;
+        }
     }
 }
