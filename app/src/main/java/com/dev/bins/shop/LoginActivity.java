98867ba3a32c51516e3a2d750cce3c608aeb679f
bin <bin6160@126.com>
2017-03-03 22:40:02 +0800

    network error save

--- app/src/main/java/com/dev/bins/shop/LoginActivity.java
+++ app/src/main/java/com/dev/bins/shop/LoginActivity.java
@@ -1,13 +1,17 @@
 package com.dev.bins.shop;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -29,7 +33,28 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+    }
 
+
+    @OnClick(R.id.btn_login)
+    public void login(View view) {
+        if (TextUtils.isEmpty(mEditTextPhone.getText().toString().trim())) {
+            mEditTextPhone.setError(getString(R.string.error_empty));
+        }
+        if (TextUtils.isEmpty(mEditTextPwd.getText().toString().trim())) {
+            mEditTextPwd.setError(getString(R.string.error_empty));
+        }
+    }
+
+    @OnClick(R.id.tv_forget_pwd)
+    public void forgetPwd(View view) {
+
+    }
+
+    @OnClick(R.id.tv_regist)
+    public void regist(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 
 
