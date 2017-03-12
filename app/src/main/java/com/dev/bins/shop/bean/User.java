1b4589948f966d451fba8961d3667f58dc38380d
bin <bin6160@126.com>
2017-03-12 09:51:31 +0800

    add user bean

---
+++ app/src/main/java/com/dev/bins/shop/bean/User.java
@@ -0,0 +1,36 @@
+package com.dev.bins.shop.bean;
+
+/**
+ * Created by bin on 12/03/2017.
+ */
+
+public class User {
+
+    private String phone;
+    private String pwd;
+    private String avatar;
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+}
