03f5500229e4c0deb088bb2a7cb68db3945653fe
bin <bin6160@126.com>
2017-03-14 09:26:30 +0800

    restructure response msg

---
+++ app/src/main/java/com/dev/bins/shop/bean/ResponseMsg.java
@@ -0,0 +1,37 @@
+package com.dev.bins.shop.bean;
+
+/**
+ * Created by bin on 14/03/2017.
+ */
+
+public class ResponseMsg<T> {
+
+    private int errorCode;
+    private String errorMsg;
+
+    private T data;
+
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(int errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorMsg() {
+        return errorMsg;
+    }
+
+    public void setErrorMsg(String errorMsg) {
+        this.errorMsg = errorMsg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
